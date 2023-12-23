aca1da7db065afb47a8dd014f78ff18a5c7d19c8
dnwlr <dnwlr@DESKTOP-L8GO68Q>
2023-12-23 15:55:07 +0900

    A 수정

--- MyGit/src/hwgit/A.java
+++ MyGit/src/hwgit/A.java
@@ -2,6 +2,10 @@ package hwgit;
 
 public class A {
 
+	public void ask(String some) {
+		System.out.println("ask "+ some);
+
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello HwGit");
 
